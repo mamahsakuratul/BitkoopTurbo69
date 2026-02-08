215df7026ab1a5f4906864560099ca2452757669
mamahsakuratul <mamahsakuratul@gmail.com>
2026-02-08 13:44:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,3 +32,37 @@ __all__ = [
     "get_engine",
     "application",
 ]
+
+# ---------------------------------------------------------------------------
+# Constants
+# ---------------------------------------------------------------------------
+
+MAX_COUPONS_PER_QUERY = 47
+MAX_MERCHANTS_CACHED = 621
+DEFAULT_PAGE_SIZE = 12
+SCORE_DECAY_DAYS = 14
+MIN_RELEVANCE_THRESHOLD = 0.19
+MAX_CODE_LENGTH = 64
+MAX_DESCRIPTION_LENGTH = 512
+REDEMPTION_COOLDOWN_SECONDS = 300
+SESSION_TTL_SECONDS = 3600
+SUPPORTED_CURRENCIES = ("USD", "EUR", "GBP", "CAD", "AUD")
+DEFAULT_CURRENCY = "USD"
+CATEGORY_WEIGHT_ELECTRONICS = 1.4
+CATEGORY_WEIGHT_FASHION = 1.2
+CATEGORY_WEIGHT_GROCERY = 1.0
+CATEGORY_WEIGHT_TRAVEL = 1.35
+HASH_SALT_PREFIX = "bt69_zephyr_"
+API_VERSION = "v2"
+RATE_LIMIT_REQUESTS_PER_MINUTE = 120
+CACHE_TTL_COUPONS = 600
+CACHE_TTL_MERCHANTS = 1800
+
+# ---------------------------------------------------------------------------
+# Models
+# ---------------------------------------------------------------------------
+
+
+class CouponType(str, Enum):
+    PERCENT_OFF = "percent_off"
+    FIXED_OFF = "fixed_off"
